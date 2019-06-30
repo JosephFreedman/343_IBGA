bb7feab016185bfcc65535851a832e5d040ae6f1
sopheak012 <kosopheakko@gmail.com>
2019-06-30 01:41:04 -0400

    Start Game Screen
    After the user press Start Game option in the main menu, a window pops up givign the user more option.

---
+++ src/startGameScreen.java
@@ -0,0 +1,91 @@
+
+
+
+
+
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class startGameScreen extends JFrame {	
+	private JPanel mainPanel;
+
+	private GridLayout mainLayout;
+	
+	private JButton newGameButton;
+	private JButton optionButton;
+	private JButton quitButton;
+	
+	private String newGameButtonName = "newGameBtn";
+	private String optionButtonName = "optionBtn";
+	private String quitButtonName = "quitBtn";
+	
+	/**
+	 * Constructs the main menu.
+	 **/
+	protected startGameScreen()
+	{
+		setTitle("343_IBGA");
+		setPreferredSize(new Dimension(300, 300));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		
+		add(mainMenu());
+		
+		pack();
+		setVisible(true);
+		toFront();
+	}
+	
+	private JPanel mainMenu()
+	{
+		// Creates the panel
+		mainPanel = new JPanel();
+		
+		// Sets Layout
+		mainLayout = new GridLayout(3, 1, 5, 5);
+		mainPanel.setLayout(mainLayout);
+		
+		// Creates buttons
+		newGameButton = new JButton("New Game");
+		optionButton = new JButton("Option");
+		quitButton = new JButton("Quit");
+		
+		// Sets button commands
+		newGameButton.setActionCommand(newGameButtonName);
+		optionButton.setActionCommand(optionButtonName);
+		quitButton.setActionCommand(quitButtonName);
+		
+		// Adds buttons to mainPanel
+		mainPanel.add(newGameButton);
+		mainPanel.add(optionButton);
+		mainPanel.add(quitButton);
+		
+		return mainPanel;
+	}
+	
+	protected void riskViewActionListeners(ActionListener evt)
+	{
+		newGameButton.addActionListener(evt);
+		optionButton.addActionListener(evt);
+		quitButton.addActionListener(evt);
+	}
+	
+	
+	//Testing the UI
+	public static void main (String [] args) {
+		startGameScreen test = new startGameScreen();
+	}
+}
+
+
+
+	
+
